fa348ef8d2939815026249003761ce116c17e081
Haris <82910362+yunik0-h@users.noreply.github.com>
2023-11-06 14:21:42 +0100

    Creazione componente e modifica

---
+++ src/components/waqar/Logo.tsx
@@ -0,0 +1,9 @@
+const logo = () => {
+  return (
+    <>
+      <h1>Haris Waqar</h1>
+    </>
+  );
+};
+
+export default logo;
--- src/pages/pageWaqar.tsx
+++ src/pages/pageWaqar.tsx
@@ -1,7 +1,10 @@
+import Logo from "../components/waqar/Logo";
+
 const pageWaqar = () => {
   return (
     <>
       <h1>Haris Waqar</h1>
+      <Logo />
     </>
   );
 };
