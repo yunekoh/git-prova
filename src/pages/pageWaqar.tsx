cc393d84b7a2a7b154c715bf99b5ff67cba4f6c4
Haris <82910362+yunik0-h@users.noreply.github.com>
2023-11-06 14:29:05 +0100

    Aggiornamento pageWaqar con tag p

--- src/pages/pageWaqar.tsx
+++ src/pages/pageWaqar.tsx
@@ -5,6 +5,7 @@ const pageWaqar = () => {
     <>
       <h1>Haris Waqar</h1>
       <Logo />
+      <p>Ho fatto un git checkout sul branch secondario, e poi fatto git rebase di master</p>
     </>
   );
 };
