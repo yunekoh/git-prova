c667db2393b53fa6eeaa0c203c3c571541e5f0d7
Haris <82910362+yunik0-h@users.noreply.github.com>
2023-11-06 14:37:05 +0100

    Risposta domanda es 4

--- src/pages/pageWaqar.tsx
+++ src/pages/pageWaqar.tsx
@@ -5,7 +5,21 @@ const pageWaqar = () => {
     <>
       <h1>Haris Waqar</h1>
       <Logo />
-      <p>Ho fatto un git checkout sul branch secondario, e poi fatto git rebase di master</p>
+      <p>
+        Ho fatto un git checkout sul branch secondario, e poi fatto git rebase
+        di master
+      </p>
+
+      {/* DOMANDA ES 4 */}
+      <h3>
+        la mia branch dedicata è indietro di qualche commit rispetto a master,
+        come faccio a portarla avanti? Cioè, come faccio a portare le modifiche
+        fatte su master sulla mia branch dedicata?
+      </h3>
+      <p>
+        Per portare avanti la mia branch dedicata rispetto a master, posso
+        utilizzare il comando `git merge`
+      </p>
     </>
   );
 };
